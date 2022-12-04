1cac1ffe1ccf48c2803d64fc6c3571408a05e56e
halilibrahimozugur <103412720+halilibrahimozugur@users.noreply.github.com>
2022-12-04 20:18:45 +0300

    first

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/asignments.js
@@ -0,0 +1,24 @@
+/*let country = "Turkey";
+let continent = "Asia";
+let population = "80plus million"
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let javascriptIsFun = "true";
+console.log(javascriptIsFun);
+
+console.log(typeof "true");
+console.log(typeof javascriptIsFun);
+console.log(typeof true);
+console.log(typeof 23);
+
+javascriptIsFun = 'YES!';
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+*/
+
+
+
+
---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,48 @@
+/*console.log("Jonas");
+console.log(23);
+
+let firstName = "berk20075";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let $let = "asds";
+console.log($let);
+*/
+/*
+// Math Operators
+const now = 2023;
+const ageHalil = now - 1992;
+const ageOzan = now - 1990;
+console.log(ageHalil, ageOzan);
+
+console.log(ageHalil * 2, ageOzan / 10, 2 ** 3);
+
+const firstName = 'Halil';
+const lastName = 'Ozan';
+console.log(firstName + ' ' + lastName);
+
+// Asignment Operators
+let x = 10 + 5;
+x += 10; // x = x + 10
+x *= 4; // x = x * 4
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x);
+
+// Comparison Operators
+console.log(ageHalil > ageOzan); // soruyoruz, true ya da false diye sonuç veriyor (>, <, >=, <=)
+console.log(ageOzan >=18);
+
+const isFullAge = ageOzan >= 18;
+
+console.log(now - 1992 < now - 1990);
+*/
+
+const now = 2023;
+const ageHalil = now - 1992;
+const ageOzan = now - 1990;
+console.log(now - 1992 < now - 1990);
