b8dfce9aca503c9adcb4f321c7a7708bf61d24c4
halilibrahimozugur <103412720+halilibrahimozugur@users.noreply.github.com>
2022-12-24 00:34:13 +0300

    24.12.2022.12.34.ev

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ console.log(2 + 3 + 4 + '5')
 let x = '1';
 console.log(+x);
 
-*/
+
 
 const age = '18';
 if (age === 18) console.log('reşit birey');
@@ -93,3 +93,88 @@ if (favorite == 4) {
 }
 
 if (favorite !== 4) console.log('4ten iyisi yok');
+
+
+
+const hasDriverLicense = true;
+const hasGoodVision = true;
+const isTired = true;
+
+console.log(hasDriverLicense && hasGoodVision);
+console.log(hasDriverLicense || hasGoodVision);
+console.log(!hasDriverLicense);
+
+
+const shouldDrive = hasDriverLicense && hasGoodVision && !isTired;
+
+if(shouldDrive) {
+  console.log('you can drive');
+} else {
+  console.log('someone else should drive..');
+}
+
+console.log(hasDriverLicense && hasGoodVision && isTired);
+
+if(shouldDrive) {
+  console.log('you can drive');
+} else {
+  console.log('someone else should drive..');
+}
+
+
+
+const jimDolphins1 = 0.1;
+const jimDolphins2 = 0.1;
+const jimDolphins3 = 0.1;
+
+const jimKoalas1 = 0.1;
+const jimKoalas2 = 0.1;
+const jimKoalas3 = 0.1;
+
+
+const avDolphins1 = (jimDolphins1 + jimDolphins2 + jimDolphins3) / 3;
+console.log(avDolphins1)
+const avKoalas1 = (jimKoalas1 + jimKoalas2 + jimKoalas3) / 3;
+console.log(avKoalas1)
+
+let avDolphins1 = prompt('yunusların ortalamasını gir')
+let avKoalas1 = prompt('koalaların ortalamasını gir')
+
+if ((avDolphins1 > avKoalas1) && (avDolphins1 > 100)) {
+  console.log('yunuslar kazandı ve kupayı aldı')
+} else if ((avDolphins1 < avKoalas1) && (avKoalas1 > 100)) {
+  console.log('koalalar kazandı ve kupayı aldı')
+} else {
+    if ((avDolphins1 > avKoalas1) && (avDolphins1 < 100)) {
+      console.log('yunuslar kupasız kazandı')
+    }
+    else if ((avDolphins1 < avKoalas1) && (avKoalas1 > 100)) {
+      console.log('koalalar kupasız kazandı')
+    }
+    else {
+      console.log('kupayı kimse almadı, kardeşlik kazandı')
+    }
+  }
+
+
+let avDolphins1 = Number(prompt('yunusların ortalamasını gir'))
+let avKoalas1 = Number(prompt('koalaların ortalamasını gir'))
+  
+if ((avDolphins1 > avKoalas1) && (avDolphins1 > 100)) {
+    console.log('yunuslar kazandı ve kupayı aldı');
+} else if ((avDolphins1 > avKoalas1) && (avDolphins1 <= 100)) {
+    console.log('yunuslar kazandı ve babayı aldı');
+} else if ((avDolphins1 < avKoalas1) && (avKoalas1 > 100)) {
+    console.log('koalalar kazandı ve kupayı aldı');
+} else if ((avDolphins1 < avKoalas1) && (avKoalas1 <= 100)) {
+    console.log('koalalar kazandı ve babayı aldı');
+} else  {
+    if (avDolphins1, avKoalas1 > 100) { 
+      console.log('iki takım da kupayı kazandı');
+    }
+    else {
+      console.log('kupayı kimse almadı, babalar kazandı');
+    }
+  }
+
+*/
