b1a3486f2be568db2971ed5e834f8fdc30caedda
halilibrahimozugur <halilibrahimozugur@gmail.com>
2022-12-26 13:30:07 +0300

    26.12.2022.13.30.is

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -40,5 +40,40 @@ console.log(appleOrangeJuice);
 const num = Number('23');
 console.log(num);
 
-*/
 
+
+// function decleration - burada value'yu önceden tanımlayabiliyoruz, sonrasında da tanımlayabiliriz
+
+const age1 = calcAge1(1991);
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+
+
+//function expression - burada value'yu sonrada tanımlamamız gerekiyor
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+}
+const age2 = calcAge1(1991);
+
+console.log(age1, age2);
+
+
+
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearUntilRetirement = (birthYear, firstName) => {
+  const age = 2022 - birthYear;
+  const retirement = 65 - age;
+  // return ritirement;
+  return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearUntilRetirement(1991, 'Jonas'));
+console.log(yearUntilRetirement(1992, 'Halil'));
+
+*/
