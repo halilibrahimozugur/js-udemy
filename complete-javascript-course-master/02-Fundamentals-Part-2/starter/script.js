5c8e38599e8c9c3a9aceee068c7565b0b0af51f7
halilibrahimozugur <103412720+halilibrahimozugur@users.noreply.github.com>
2023-01-04 22:28:05 +0300

    10.28

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -392,4 +392,18 @@ for(let i = 0; i < halil.length; i++) {
   console.log(halil[i], typeof halil[i]);
 }
 
+// exercise örneği (Looping Backwards and Loops in Loops)
+for(let i = halil.length - 1; i >= 0; i--) {
+  console.log(i, halil[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`* Starting Exercise ${exercise} *`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: lifting weight repetition ${rep}`);
+  }
+}
+
 */
+
