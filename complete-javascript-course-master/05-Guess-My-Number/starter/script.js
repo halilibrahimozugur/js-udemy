5efef7ded0ae10256b80d756bc6ab369d38b7022
halilibrahimozugur <103412720+halilibrahimozugur@users.noreply.github.com>
2023-01-09 22:04:31 +0300

    09.01.2023

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,5 +1,45 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'denemasyon';
-console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 45;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
+*/
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 7;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Lütfen Sayı Giriniz';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Tutturdunuz BRAVO!';
+    score++;
+    document.querySelector('.score').textContent = score;
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Biraz daha azalt :)';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'Kaybettin, baştan başla';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Biraz daha yükselt :)';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'Kaybettin, baştan başla';
+    }
+  }
+});
